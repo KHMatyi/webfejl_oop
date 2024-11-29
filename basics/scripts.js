8e4bde3926d43b7dda97c43c651777e3765371e7
kovacs.matyas <kovacs.matyas@T04PC10>
2024-11-29 10:07:05 +0100

    >2015

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,3 +1,41 @@
+class Player{
+    playedMatch = 0;
+    constructor(nickname){
+        this.nickname = nickname;
+    }
+
+    play(){
+        this.playedMatch += 1;
+        console.log(this.nickname + " " + this.playedMatch);
+    }
+
+    getTier(){
+        if (this.playedMatch < 4){
+            return 'A';
+        } else if (this.playedMatch < 7){
+            return 'B';
+        } else {
+            return 'C';
+        }
+    }
+}
+
+function Person(name,){
+    this.name = name;
+}
+Person.prototype.getName = function(){
+    return this.name;
+}
+
+function Student(name, school){
+    
+}
+Object.setPrototypeOf(Student.prototype,Person.prototype)
+
+
+
+
+/*
 function Player(nickname, ){
     this.nickname = nickname;   
     this.playedMatch = 0;
@@ -15,6 +53,7 @@ Player.prototype.getTier = function(){
         return 'C';
     }
 }
+*/
 const gom = new Player("gomszab");
 
 console.log(gom);
