0f2adb7b6abbb7be87fac45e453467c4a9347df7
Kovacs-Haasz Matyas <kovacs.matyas@BOLYAI.LOCAL>
2025-01-31 15:23:24 +0100

    Óra vége

---
+++ datamanager/main.js
@@ -0,0 +1,93 @@
+/**
+ * @typedef {{nev:String ,eletkor:Number}} Person
+ * @typedef {Person[]} Persons
+ * @callback UpdateCallback(Persons)
+ */
+class DataManager{
+    /**
+     * @type {Person[]}
+     */
+    #array;
+    /**
+     * @type {UpdateCallback}
+     */
+    #updateCallback;
+    /**
+     * @param {Person[]} array
+     */
+    constructor(array = []){
+        this.#array = array;
+        this.#updateCallback = ()=>{};
+    }
+    /**
+     * @param {UpdateCallback} callback 
+     */
+    setUpdateCallback(callback){
+        this.#updateCallback = callback;
+        this.#updateCallback(this.#array);
+    }
+    /**
+     * 
+     * @param {Person} p 
+     */
+    add(p){
+        this.#array.push(p);
+        this.#updateCallback(this.#array)
+    }
+    /**
+     * @param {String} name 
+     */
+    filterName(name){
+        const results = [];
+        for (const i of this.#array){
+            if (i.nev.includes(name)){
+                results.push(i);
+            }
+        }
+        this.#updateCallback(results)
+    }
+    /**
+     * 
+     * @param {Number} age 
+     */
+    filterName(age){
+        const results = [];
+        for (const i of this.#array){
+            if (i.eletkor == age){
+                results.push(i);
+            }
+        }
+        this.#updateCallback(results)
+    }
+}
+class DataTable{
+    /**
+     * 
+     * @param {DataManager} dataManager 
+     */
+    constructor(dataManager){
+        const table = document.createElement("table");
+        document.body.appendChild(table);
+        const tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+
+        dataManager.setUpdateCallback((Persons)=>{
+            tbody.innerHTML = "";
+            for (const i of Persons){
+                const tr = document.createElement("tr");
+                tbody.appendChild(tr);
+                
+                const td1 = document.createElement("td");
+                tr.appendChild(td1);
+                const td2 = document.createElement("td");
+                tr.appendChild(td2);
+                td1.innerHTML = i.nev;
+                td2.innerHTML = i.eletkor;
+            }
+        });
+    }
+}
+
+const data = [{nev:"jani",eletkor:34},{nev:"hehe",eletkor:334}]
+const dm = new DataManager(data);
+const dt = new DataTable(dm);
